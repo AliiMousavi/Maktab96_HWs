d1763f0f345de9fee49f7cadba4a7050e8159244
AliiMousavi <AliMousavi04897@gmail.com>
2023-07-06 05:09:46 +0330

    create and implement Thread1.java

---
+++ Hw11_Thread/src/main/java/q2/threads/Thread1.java
@@ -0,0 +1,28 @@
+package main.java.q2.threads;
+
+public class Thread1 implements Runnable{
+
+    private final String firstLock;
+    private final String secondLock;
+
+    public Thread1(String firstLock, String secondLock) {
+        this.firstLock = firstLock;
+        this.secondLock = secondLock;
+    }
+
+    @Override
+    public void run() {
+
+        synchronized(firstLock) {
+            System.out.println("Thread1 get firstLock");
+
+            try {
+                Thread.sleep(2000);
+                secondLock.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
