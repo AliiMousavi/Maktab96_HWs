f56b891c0eb00c9b298db606a5d4f4a4c5103b9c
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-10 04:35:26 +0330

    implement PersonRepositoryImpl.java(hw14)

---
+++ hw14_jpa/src/main/java/repository/impl/PersonRepositoryImpl.java
@@ -0,0 +1,83 @@
+package repository.impl;
+
+import entity.Person;
+import repository.PersonRepository;
+
+import javax.persistence.EntityManager;
+import java.util.Collection;
+import java.util.Optional;
+
+public class PersonRepositoryImpl implements PersonRepository {
+
+    protected EntityManager em;
+
+    public PersonRepositoryImpl(EntityManager em) {
+        this.em = em;
+    }
+
+
+    @Override
+    public Person saveOrUpdate(Person person) {
+        beginTransaction();
+        Person person1 = saveWithoutTransaction(person);
+        commitTransaction();
+        em.clear();
+        return person1;
+    }
+
+
+    private Person saveWithoutTransaction(Person person) {
+        if (person.getId() == null)
+            em.persist(person);
+        else
+            person = em.merge(person);
+
+        return person;
+    }
+
+    @Override
+    public Person update(Person person) {
+        person = em.merge(person);
+        return person;
+    }
+
+    @Override
+    public Person load(Person person) {
+        return em.find(Person.class, person);
+    }
+
+    @Override
+    public Collection<Person> loadAll() {
+        return em.createQuery("from " + Person.class.getSimpleName(), Person.class).getResultList();
+    }
+
+    @Override
+    public boolean contains(Person person) {
+        return isPersonExist(em.find(Person.class, person));
+    }
+
+    @Override
+    public void beginTransaction() {
+        if (!em.getTransaction().isActive())
+            em.getTransaction().begin();
+    }
+
+    @Override
+    public void commitTransaction() {
+        if (em.getTransaction().isActive()) {
+            em.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void rollBack() {
+        if (em.getTransaction().isActive())
+            em.getTransaction().rollback();
+
+    }
+
+    private boolean isPersonExist(Person person){
+        return person != null;
+
+    }
+}
