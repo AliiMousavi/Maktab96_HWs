76b40b6003d3f89ca2aa1125cae112ccb26e0ee0
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-10 04:34:27 +0330

    implement Person.java (hw14)

---
+++ hw14_jpa/src/main/java/entity/Person.java
@@ -0,0 +1,42 @@
+package entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Table(name = "persons")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    @Column(name = "first_name")
+    String firstName;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    @Column(name = "date_of_birth")
+    Date dateOfBirth;
+
+    public Person(String firstName, String lastName, Date dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
