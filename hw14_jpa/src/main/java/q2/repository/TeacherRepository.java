e364f20f2499c10cf883137c67f8c7a7a0277932
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-10 07:59:26 +0330

    edite load method in TeacherRepository.java(hw14)

--- hw14_jpa/src/main/java/q2/repository/TeacherRepository.java
+++ hw14_jpa/src/main/java/q2/repository/TeacherRepository.java
@@ -7,9 +7,10 @@ import java.util.Collection;
 public interface TeacherRepository {
     Teacher saveOrUpdate(Teacher teacher);
     Teacher update(Teacher teacher);
-    Teacher load(Teacher teacher);
+    void deleteById(Long id);
+    Teacher load(Long id);
     Collection<Teacher> loadAll();
-    boolean contains(Teacher teacher);
+    boolean contains(Long id);
 
     void beginTransaction();
 
