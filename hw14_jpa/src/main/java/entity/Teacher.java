79f5b3816179071d6bdb9428bb6ad5b97ab8bcf3
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-10 04:37:58 +0330

    implement Teacher.java(hw14)

---
+++ hw14_jpa/src/main/java/entity/Teacher.java
@@ -0,0 +1,27 @@
+package entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class Teacher extends Person{
+    @Column(name = "teacher_id")
+//    @NotNull(message = "Teacher Id cannot be null")
+//    @Size(min = 10, max = 10, message
+//            = "Teacher Id must be 10 characters")
+    Long teacherId;
+    String degree;
+    Masterdegree masterdegree;
+
+    public Teacher(String firstName, String lastName, Long teacherId) {
+        super(firstName, lastName);
+        this.teacherId = teacherId;
+    }
+}
