acc541a12e639cb80839479c380bbc207a59bec2
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-10 08:01:14 +0330

    run the program(hw14)

--- hw14_jpa/src/main/java/q2/Main.java
+++ hw14_jpa/src/main/java/q2/Main.java
@@ -1,18 +1,25 @@
 package q2;
 
 import q2.entity.Person;
+import q2.ui.Menu;
 import q2.util.ApplicationContext;
 
 import java.sql.Date;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
 
         Person person = new Person("ali" , "Mousavi" , Date.valueOf("2001-08-17"));
+        Person person1 = new Person(2L,"alii" , "Mousavi" , Date.valueOf("2001-08-17"));
 
-        ApplicationContext.getPersonService().saveOrUpdate(person);
+        Menu menu = new Menu();
+        menu.run();
+
+
+//        ApplicationContext.getPersonService().saveOrUpdate(person);
 //        ApplicationContext.getPersonService().signUp(person.getFirstName(), person.getLastName());
-//        System.out.println(ApplicationContext.getPersonService().contains(person));
+//        System.out.println(ApplicationContext.getPersonService().contains(person1.getId()));
 
     }
 }
