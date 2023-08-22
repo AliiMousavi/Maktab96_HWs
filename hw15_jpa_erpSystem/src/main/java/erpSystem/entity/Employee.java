7ee7c4c2cfc99a0ac8103154e836caed19527e13
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-22 03:31:38 +0330

    implement Employee.java (hw15)

---
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/entity/Employee.java
@@ -0,0 +1,18 @@
+package erpSystem.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Employee extends User{
+    int salary;
+}
