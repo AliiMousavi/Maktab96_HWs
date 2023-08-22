e811b326c9ae6e93419da1c99cb7c158a1df2fe4
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-22 03:32:59 +0330

    implement LogIn.java (hw15)

---
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/entity/LogIn.java
@@ -0,0 +1,19 @@
+package erpSystem.entity;
+
+import erpSystem.base.entity.BaseEntity;
+import erpSystem.entity.enums.LogInRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LogIn extends BaseEntity<Long> {
+    LogInRole logInRole;
+    String username;
+    String password;
+}
