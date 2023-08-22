4040fbcfcafa0c88860f249b378ae4ae42806632
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-22 16:52:24 +0330

    implement StudentService.java (hw15)

---
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/service/StudentService.java
@@ -0,0 +1,8 @@
+package erpSystem.service;
+
+import erpSystem.base.service.BaseService;
+import erpSystem.entity.Student;
+
+public interface StudentService extends BaseService<Student,Long> {
+    
+}
