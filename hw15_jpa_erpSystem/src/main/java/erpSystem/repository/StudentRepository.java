c4d6406ac5cb278548d072c1c251a676452b0190
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-22 16:52:00 +0330

    implement StudentRepository.java (hw15)

---
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package erpSystem.repository;
+
+import erpSystem.base.repository.BaseRepository;
+import erpSystem.entity.Student;
+
+public interface StudentRepository extends BaseRepository<Student,Long> {
+
+}
