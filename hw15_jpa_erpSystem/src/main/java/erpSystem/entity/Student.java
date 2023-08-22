696abd8102761fca878a1ac89af57dd8741dbdad
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-22 16:51:26 +0330

    add constructor in Student.java (hw15)

--- hw15_jpa_erpSystem/src/main/java/erpSystem/entity/Student.java
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/entity/Student.java
@@ -1,5 +1,10 @@
 package erpSystem.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
@@ -7,10 +12,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Student extends User{
 
+    float GpaOfPreviousSemester;
     @ManyToMany
-    List<Course> courseList = new ArrayList<>();
+    List<Course> passedCourses = new ArrayList<>();
 
 
 }
