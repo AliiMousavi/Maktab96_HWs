5c173199c5acd8f728934568f8b5a913ac06e644
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-22 03:32:21 +0330

    implement EducationDepartment.java (hw15)

---
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/entity/EducationDepartment.java
@@ -0,0 +1,10 @@
+package erpSystem.entity;
+
+
+public class EducationDepartment extends Employee{
+     private String iHaveAmassageForEducationDepartment;
+
+    public EducationDepartment() {
+        this.iHaveAmassageForEducationDepartment = "khoda ghovat";
+    }
+}
