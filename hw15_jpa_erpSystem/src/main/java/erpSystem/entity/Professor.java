b302402fb4c2c8201d0311a03ce41d0b5866d29b
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-24 06:32:22 +0330

    Add required fiels to Professor.java (hw15)

--- hw15_jpa_erpSystem/src/main/java/erpSystem/entity/Professor.java
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/entity/Professor.java
@@ -6,12 +6,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Professor extends Employee{
+public class Professor extends Employee {
+
+    @OneToMany(mappedBy = "professor")
+    List<Course> Courses = new ArrayList<>();
 
     ProfessorType professorType;
+
 }
