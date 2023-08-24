d11e4c06ee29b2de6ba925d2c7a6b66d4969917a
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-24 06:32:50 +0330

    implement ApplicationContext.java(hw15)

--- hw15_jpa_erpSystem/src/main/java/erpSystem/util/ApplicationContext.java
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/util/ApplicationContext.java
@@ -1,26 +1,78 @@
 package erpSystem.util;
 
 import erpSystem.config.EntityManagerProvider;
-import erpSystem.repository.StudentRepository;
-import erpSystem.repository.impl.StudentRepositoryImpl;
-import erpSystem.service.StudentService;
-import erpSystem.service.impl.StudentServiceImpl;
+import erpSystem.entity.Course;
+import erpSystem.entity.Student;
+import erpSystem.entity.User;
+import erpSystem.repository.*;
+import erpSystem.repository.impl.*;
+import erpSystem.service.*;
+import erpSystem.service.impl.*;
 
 import javax.persistence.EntityManager;
 
 public class ApplicationContext {
 
+    private static final EntityManager em;
+
+    private static final UserRepository<User> userRepository;
+    private static final UserService<User> userService;
+
     private static final StudentRepository studentRepository;
     private static final StudentService studentService;
-    static final EntityManager em;
+
+    private static final CourseRepository courseRepository;
+    private static final CourseService courseService;
+
+    private static final ProfessorRepository professorRepository;
+    private static final ProfessorService professorService;
+
+    private static final EducationDepartmentRepository educationDepartmentRepository;
+    private static final EducationDepartmentService educationDepartmentService;
+
+
 
     static {
         em = EntityManagerProvider.getEntityManager();
+
+        userRepository = new UserRepositoryImpl<>(em) {
+            @Override
+            public Class<User> getEntityClass() {
+                return User.class;
+            }
+        };
+        userService = new UserServiceImpl<>(userRepository);
+
         studentRepository = new StudentRepositoryImpl(em);
         studentService = new StudentServiceImpl(studentRepository);
+
+        courseRepository = new CourseRepositoryImpl(em);
+        courseService = new CourseServiceImpl(courseRepository);
+
+        professorRepository = new ProfessorRepositoryImpl(em);
+        professorService = new ProfessorServiceImpl(professorRepository);
+
+        educationDepartmentRepository = new EducationDepartmentRepositoryImpl(em);
+        educationDepartmentService = new EducationDepartmentServiceImpl(educationDepartmentRepository);
+
     }
 
     public static StudentService getStudentService (){
         return studentService;
+    }
+
+    public static ProfessorService getProfessorService(){
+        return professorService;
+    }
+
+    public static EducationDepartmentService getEducationDepartmentService(){
+        return educationDepartmentService;
+    }
+
+    public static UserService<User> getUserService(){
+        return userService;
+    }
+    public static CourseService getCourseService(){
+        return courseService;
     }
 }
