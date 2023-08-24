dcb746e46914429fdf798ebac8a82af1f77b2c48
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-24 06:30:03 +0330

    implement ProfessorRepository.java (hw15)

---
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/repository/ProfessorRepository.java
@@ -0,0 +1,8 @@
+package erpSystem.repository;
+
+import erpSystem.base.repository.BaseRepository;
+import erpSystem.entity.Professor;
+import erpSystem.entity.Student;
+
+public interface ProfessorRepository extends UserRepository<Professor> {
+}
