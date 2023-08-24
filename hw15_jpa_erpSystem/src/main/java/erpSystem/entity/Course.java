54f725a978ce20ec3f7cf394f421e6ac5293fa8e
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-24 06:26:25 +0330

    add fields to Course.java (hw15)

--- hw15_jpa_erpSystem/src/main/java/erpSystem/entity/Course.java
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/entity/Course.java
@@ -5,11 +5,22 @@ import erpSystem.base.entity.BaseEntity;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Course extends BaseEntity<Long> {
-    @ManyToMany(mappedBy = "passedCourses" , cascade = CascadeType.PERSIST)
+
+    String name;
+    int unit;
+
+    @ManyToOne
+    Collage collage;
+
+    @ManyToMany
     List<Student> students = new ArrayList<>();
+
+    @ManyToOne
+    Professor professor;
 }
