ac76b88e5e6a90a470137da5347e13dc2ec9bd19
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-24 06:25:56 +0330

    add Constants to Constant.java (hw15)

---
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/util/Constant.java
@@ -0,0 +1,17 @@
+package erpSystem.util;
+
+public class Constant {
+
+
+    public static String[] startupItem={"login","sign up","exit"};
+    public static String[] edItem={"Student Setting","Professor Setting","Employee Setting" , "Courses Setting" , "View My Payslip" , "Log out."};
+    public static String[] edSettingItem={"Register","Delete","Edite" , "back!"};
+    public static String[] studentItem={"View My Profile","View My Courses","Take Courses" , "View Passed Courses" , "Log out."};
+    public static String[] professorItem={"View My Profile","Enter Student Grades","View My Payslip", "Log out."};
+    public static String[] editeStudentItem={ "Edite Username","Edite Email" , "Edite Password", "Back!."};
+
+
+
+    public static int baseSalary = 3000;
+
+}
