23329c49ca7880bd114d0f6501957ed021c990fc
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-24 06:18:16 +0330

    add extends UserRepository to StudentRepository.java class  (hw15)

--- hw15_jpa_erpSystem/src/main/java/erpSystem/repository/StudentRepository.java
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/repository/StudentRepository.java
@@ -3,6 +3,8 @@ package erpSystem.repository;
 import erpSystem.base.repository.BaseRepository;
 import erpSystem.entity.Student;
 
-public interface StudentRepository extends BaseRepository<Student,Long> {
+import java.util.Optional;
+
+public interface StudentRepository extends UserRepository<Student> {
 
 }
