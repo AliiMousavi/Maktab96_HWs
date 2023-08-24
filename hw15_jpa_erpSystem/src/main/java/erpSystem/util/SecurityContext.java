f5f382bab6e5e9b48807197b6bdb8a3cc0378ab0
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-24 06:39:17 +0330

    delete SecurityContext.java(hw15)

--- hw15_jpa_erpSystem/src/main/java/erpSystem/util/SecurityContext.java
+++
@@ -1,8 +0,0 @@
-package erpSystem.util;
-
-public class SecurityContext {
-    public static Long id;
-    public static String username;
-    public static String email;
-    public static String password;
-}
