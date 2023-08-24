3adff9d105d520ec0d288a950a0d68dbbb9c5682
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-24 06:28:09 +0330

    implement EducationDepartmentService.java (hw15)

---
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/service/EducationDepartmentService.java
@@ -0,0 +1,8 @@
+package erpSystem.service;
+
+import erpSystem.base.service.BaseService;
+import erpSystem.entity.EducationDepartment;
+import erpSystem.entity.Professor;
+
+public interface EducationDepartmentService extends UserService<EducationDepartment> {
+}
