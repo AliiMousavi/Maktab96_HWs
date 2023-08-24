d381760f3af6f495a9dceb856c6f29fb34377849
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-24 06:25:29 +0330

    add Courses to Collage.java (hw15)

--- hw15_jpa_erpSystem/src/main/java/erpSystem/entity/Collage.java
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/entity/Collage.java
@@ -4,11 +4,22 @@ import erpSystem.base.entity.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Collage extends BaseEntity<Long> {
-    String AcademicYear;
-    String AcademicSemester;
+    String correntYear;
+    String correntSemester;
+
+    @OneToMany(mappedBy = "collage")
+    List<Course> CollageCourseList = new ArrayList<>();
 }
