f823198abfb4e2c0e5a67afd260aeaf75c7addc5
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-24 06:19:28 +0330

    add extends UserServiceImpl to StudentServiceImpl.java class  (hw15)

--- hw15_jpa_erpSystem/src/main/java/erpSystem/service/impl/StudentServiceImpl.java
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/service/impl/StudentServiceImpl.java
@@ -3,11 +3,14 @@ package erpSystem.service.impl;
 import erpSystem.base.service.impl.BaseServiceImpl;
 import erpSystem.entity.Student;
 import erpSystem.repository.StudentRepository;
+import erpSystem.repository.UserRepository;
 import erpSystem.service.StudentService;
 
-public class StudentServiceImpl extends BaseServiceImpl<Student,Long, StudentRepository>
+import java.util.Optional;
+
+public class StudentServiceImpl extends UserServiceImpl<Student>
         implements StudentService {
-    public StudentServiceImpl(StudentRepository repository) {
+    public StudentServiceImpl(UserRepository<Student> repository) {
         super(repository);
     }
 }
