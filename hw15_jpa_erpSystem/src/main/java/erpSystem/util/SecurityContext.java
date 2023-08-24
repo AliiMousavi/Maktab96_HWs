4dc438a8822cad9cc8fdac0761ed615f8f94fd78
AliiMousavi <AliMousavi04897@gmail.com>
2023-08-24 06:34:15 +0330

    implement SecurityContext.java(hw15)

---
+++ hw15_jpa_erpSystem/src/main/java/erpSystem/util/SecurityContext.java
@@ -0,0 +1,8 @@
+package erpSystem.util;
+
+public class SecurityContext {
+    public static Long id;
+    public static String username;
+    public static String email;
+    public static String password;
+}
