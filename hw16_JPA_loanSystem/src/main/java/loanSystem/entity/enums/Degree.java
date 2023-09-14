6faba178af88e03139259a3baf3866d2562d5c62
AliiMousavi <AliMousavi04897@gmail.com>
2023-09-15 01:32:09 +0330

    implement Degree.java (hw16)

---
+++ hw16_JPA_loanSystem/src/main/java/loanSystem/entity/enums/Degree.java
@@ -0,0 +1,13 @@
+package loanSystem.entity.enums;
+
+public enum Degree {
+    KARDANI,
+    KARSHENASI,
+    ARSHADPEYVASTE,
+    ARSHADNOPEYVASTE,
+    HERFEIIDOCTORA,
+    PEYVASTEDOCTORA,
+    NAPEYVASTEDOCTORA,
+    TAKHASOSINAPEYVASTEDOCTORA
+
+}
