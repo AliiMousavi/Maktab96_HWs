ecc1e203a6323dd7efbdaeaf13dbb02ede06909a
AliiMousavi <AliMousavi04897@gmail.com>
2023-09-15 01:37:13 +0330

    run the main (hw16)

--- hw16_JPA_loanSystem/src/main/java/loanSystem/Main.java
+++ hw16_JPA_loanSystem/src/main/java/loanSystem/Main.java
@@ -6,13 +6,6 @@ import loanSystem.util.ApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-//        Student student = new Student();
-//        student.setFirstName("ali");
-//        ApplicationContext.getStudentService().saveOrUpdate(student);
-
-//        Student studentByUsername = ApplicationContext.getStudentService().getStudentByUsername("2283244897");
-//        System.out.println(studentByUsername.getDegree());
-
         MainMenu.run();
     }
 }
