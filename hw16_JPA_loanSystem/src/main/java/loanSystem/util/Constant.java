af130e3d2c92da372d1ad8edc8a4a4ff61732987
AliiMousavi <AliMousavi04897@gmail.com>
2023-09-15 01:31:59 +0330

    implement Constant.java (hw16)

---
+++ hw16_JPA_loanSystem/src/main/java/loanSystem/util/Constant.java
@@ -0,0 +1,11 @@
+package loanSystem.util;
+
+public class Constant {
+
+    public static String[] startupItem={"sign up","login","exit"};
+    public static String[] loanItem={"registration","repayment","back!"};
+    public static String[] loanRegistrationItem={"Shahriye","tahsili","maskan","back!"};
+    public static String[] loanRepaymentItem={"Pardakht Shode","Pardakht Na Shode","Pardakht","back!"};
+    public static String[] bigCitys={"gilan","esfahan","azarbayijansharqi","fars" , "khoozestan" , "qom" , "khorasan","alborz"};
+
+}
