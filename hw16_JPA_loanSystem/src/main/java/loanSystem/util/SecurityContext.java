0b6f1b0ed2420dba7c05fb129f67729acc4d4025
AliiMousavi <AliMousavi04897@gmail.com>
2023-09-15 01:35:30 +0330

    implement SecurityContext.java (hw16)

---
+++ hw16_JPA_loanSystem/src/main/java/loanSystem/util/SecurityContext.java
@@ -0,0 +1,10 @@
+package loanSystem.util;
+
+public class SecurityContext {
+    public static Long id;
+
+    public static String username;
+
+    public static String email;
+    public static String firstName;
+}
