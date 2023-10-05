2f7567b69b9f3b296c46593e114eca415991a531
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-05 06:24:15 +0330

    add Masterdegree.java for q1 (hw17)

--- hw14_jpa/src/main/java/q2/entity/Masterdegree.java
+++
@@ -1,9 +0,0 @@
-package q2.entity;
-
-public enum Masterdegree {
-    NEMIDOUNAM1,
-    NEMIDOUNAM2,
-    NEMIDOUNAM3,
-
-
-}
--- hw17_log_io/src/main/java/q1/entity/Masterdegree.java
+++
@@ -1,9 +0,0 @@
-package q1.entity;
-
-public enum Masterdegree {
-    NEMIDOUNAM1,
-    NEMIDOUNAM2,
-    NEMIDOUNAM3,
-
-
-}
