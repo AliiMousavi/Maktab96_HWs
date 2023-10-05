4d3865c0c35d1ce9092d237c2ca6dd8430124f0a
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-05 06:26:51 +0330

    add StudentCourseRatingRepositoryimpl.java for q2 (hw17)

--- hw17_log_io/src/main/java/q2/repository/StudentCourseRatingRepository.java
+++
@@ -1,7 +0,0 @@
-package q2.repository;
-
-import q2.base.repository.BaseRepository;
-import q2.entity.StudentCourseRating;
-
-public interface StudentCourseRatingRepository extends BaseRepository<StudentCourseRating,Long> {
-}
